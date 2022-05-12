9b60438a93589ecd724e3770dfd712bf7d7b9d6c
abiolaakinnubi <abiola.akinnubi@gmail.com>
2022-05-11 23:30:21 -0500

    Attempted missing number

---
+++ src/easy/missingnumber/Solution.java
@@ -0,0 +1,34 @@
+package easy.missingnumber;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class Solution {
+    public static void main(String[] args) {
+       var nums = new int[] {0,1};
+       missingNumber(nums);
+    }
+    public static int missingNumber(int[] nums) {
+        int n  = nums.length;
+        var numbers = IntStream.range(0,n+1)
+                .boxed()
+                .collect(Collectors.toSet());
+        for (var _num_: nums
+             ) {
+            numbers.remove(_num_);
+        }
+       return numbers.iterator().next();
+    }
+
+//    public static int missingNumber2(int[] nums) {
+//        int n  = nums.length;
+//
+//        for(var i =0; i< n; i++){
+//            if(i != d[i]){
+//                return i;
+//            }
+//        }
+//       return 0;
+//    }
+}
